1dfca59687d2304845c51b4a83bb9ce5b3bf1e8d
AntonD3 <antonv.dyadyuk@gmail.com>
2022-09-17 21:09:17 +0200

    Transtions serialization

--- compression/src/lib.rs
+++ compression/src/lib.rs
@@ -2,8 +2,11 @@
 //! The circuit free implementation of compressing and uncompressing
 //!
 
+pub const ADDRESS_SIZE: u8 = 20;
+pub const KEY_VALUE_SIZE: u8 = 32;
+
 ///
-/// Storage transition implemention
+/// Storage transition rust representation.
 ///
 pub struct StorageTransition {
     /// The account address.
@@ -14,4 +17,21 @@ pub struct StorageTransition {
     pub value: [u8; 32],
     /// The information for dynamic types encoding(optional).
     pub meta: Option<([u8; 32], [u8; 32])>,
+}
+
+impl StorageTransition {
+    ///
+    /// Converts storage transitions array into bytes.
+    ///
+    pub fn into_bytes(transitions: Vec<Self>) -> Vec<u8> {
+        let mut result = Vec::with_capacity(transitions.len() * (2*KEY_VALUE_SIZE + ADDRESS_SIZE));
+
+        for transition in transitions {
+            result.extend(transition.address);
+            result.extend(transition.key);
+            result.extend(transition.value);
+        }
+
+        result
+    }
 }
