180e7aa64fab625ceca02e4d29c3ce3dea5cd5bc
AntonD3 <antonv.dyadyuk@gmail.com>
2022-09-18 06:26:43 +0200

    circuit finished

--- circuits/src/main_circuit.rs
+++ circuits/src/main_circuit.rs
@@ -27,8 +27,8 @@ pub use franklin_crypto::{
 };
 use franklin_crypto::plonk::circuit::Assignment;
 
-pub const MAX_COMPRESSED_DATA_SIZE: usize = 100;
-pub const MAX_UNCOMPRESSED_DATA_SIZE: usize = 100;
+pub const MAX_COMPRESSED_DATA_SIZE: usize = 350;
+pub const MAX_UNCOMPRESSED_DATA_SIZE: usize = 300;
 pub const MAX_WORDS: usize = 10;
 
 pub struct CompressionCircuit<E: Engine> {
@@ -52,11 +52,11 @@ impl<E: Engine> Circuit<E> for CompressionCircuit<E> {
         let range_table_name = range_table.functional_name();
         cs.add_table(range_table)?;
 
-        let compressed_data_hash_bytes = allocate_and_prove_bytes(&self.compressed_data_hash, cs, range_table_name.as_str(), true);
-        let data_hash = allocate_and_prove_bytes(&self.data_hash, cs, range_table_name.as_str(), true);
+        let compressed_data_hash_bytes = allocate_and_prove_bytes(&self.compressed_data_hash, 32, cs, range_table_name.as_str(), true)?;
+        let data_hash = allocate_and_prove_bytes(&self.data_hash, 32, cs, range_table_name.as_str(), true)?;
 
-        let compressed_data_bytes = allocate_and_prove_bytes(&self.compressed_data, cs, range_table_name.as_str(), false);
-        let data_bytes = allocate_and_prove_bytes(&self.data, cs, range_table_name.as_str(), false);
+        let compressed_data_bytes = allocate_and_prove_bytes(&self.compressed_data, MAX_COMPRESSED_DATA_SIZE, cs, range_table_name.as_str(), false)?;
+        let data_bytes = allocate_and_prove_bytes(&self.data, MAX_UNCOMPRESSED_DATA_SIZE, cs, range_table_name.as_str(), false)?;
 
         // TODO: prove hashes correctness
 
@@ -65,19 +65,71 @@ impl<E: Engine> Circuit<E> for CompressionCircuit<E> {
             self.compressed_data_len
         )?;
 
-        let ptr = cs.get_explicit_zero();
+        let ptr = Num::alloc(
+            cs,
+            Some(E::Fr::from_str("0").unwrap())
+        )?;
+
+        let zero = Num::alloc(
+            cs,
+            Some(E::Fr::from_str("0").unwrap())
+        )?;
+
+        let one = Num::alloc(
+            cs,
+            Some(E::Fr::from_str("1").unwrap())
+        )?;
 
         for word in 0..MAX_WORDS {
+            let compressed_word = get_word_from_bytes(cs, &compressed_data_bytes, &ptr)?;
+            assert_eq!(compressed_word.len(), 33);
+            let (uncompressed_pos, size) = crate::utils::get_word_position_in_data(word);
+            let mut ok = Boolean::alloc(cs, Some(false))?;
+            if size == 20 {
+                let mut is1 = Num::equals(cs,&compressed_word[0].inner, &one)?;
+                for i in 0..20 {
+                    let eq = Num::equals(cs, &data_bytes[uncompressed_pos + i].inner, &compressed_word[i].inner)?;
+                    is1 = Boolean::and(cs, &is1, &eq)?;
+                }
+                ok = is1;
+            } else {
+                let mut is0 = Num::equals(cs,&compressed_word[0].inner, &zero)?;
+                for i in 0..32 {
+                    let eq = Num::equals(cs, &data_bytes[uncompressed_pos + i].inner, &compressed_word[i + 1].inner)?;
+                    is0 = Boolean::and(cs, &is0, &eq)?;
+                }
+                ok = Boolean::or(cs, &ok, &is0)?;
 
+                for i in 11..=32 {
+                    let i_num = Num::alloc(
+                        cs,
+                        Some(E::Fr::from_str(&format!("{}", i)).unwrap())
+                    )?;
+                    let mut is = Num::equals(cs,&compressed_word[0].inner, &i_num)?;
+                    for index in 0..32 {
+                        let eq;
+                        if index < i - 10 {
+                            eq = Num::equals(cs, &data_bytes[uncompressed_pos + index].inner, &zero)?;
+                        } else {
+                            eq = Num::equals(cs, &data_bytes[uncompressed_pos + index].inner, &compressed_word[index - (i - 10) + 1].inner)?;
+                        }
+                        is = Boolean::and(cs, &is,&eq)?;
+                    }
+                    ok = Boolean::or(cs, &ok, &is)?;
+                }
+            }
+            let true_bool = Boolean::alloc(cs, Some(true))?;
+            Boolean::enforce_equal(cs, &ok, &true_bool)?;
         }
         Ok(())
     }
 }
 
-fn allocate_and_prove_bytes<E: Engine, CS: ConstraintSystem<E>>(bytes: &Vec<Option<u8>>, cs: &mut CS, range_table_name: &str, alloc_as_inputs: bool) -> Result<Vec<Byte<E>>, SynthesisError> {
+fn allocate_and_prove_bytes<E: Engine, CS: ConstraintSystem<E>>(bytes: &Vec<Option<u8>>, len: usize, cs: &mut CS, range_table_name: &str, alloc_as_inputs: bool) -> Result<Vec<Byte<E>>, SynthesisError> {
     let mut result = Vec::with_capacity(bytes.len());
 
-    for byte in bytes {
+    for i in 0..len {
+        let byte = bytes.get(i).unwrap_or(&Some(0));
         let inner = if alloc_as_inputs {
             Num::Variable(AllocatedNum::alloc_input(cs, || Ok(E::Fr::from_str(&format!("{}", byte.unwrap())).unwrap()))?)
         } else {
@@ -108,6 +160,30 @@ fn allocate_and_prove_bytes<E: Engine, CS: ConstraintSystem<E>>(bytes: &Vec<Opti
 
 
         result.push(Byte{inner});
+    }
+
+    Ok(result)
+}
+
+fn get_word_from_bytes<E: Engine, CS: ConstraintSystem<E>>(cs: &mut CS, bytes: &Vec<Byte<E>>, pos: &Num<E>) -> Result<Vec<Byte<E>>, SynthesisError> {
+    let mut result = Vec::with_capacity(33);
+    for index in 0..33 {
+        let zero = Num::alloc(
+            cs,
+            Some(E::Fr::from_str("0").unwrap())
+        )?;
+        let res_byte = Byte::from_num(cs, zero)?;
+        for (i, byte) in bytes.iter().enumerate() {
+            let i_num = Num::alloc(
+                cs,
+                Some(E::Fr::from_str(&format!("{}", i)).unwrap())
+            )?;
+            let flag = Num::equals(cs,&i_num, pos)?;
+            let flag = Num::from_boolean_is(flag);
+            flag.mul(cs, &byte.inner);
+            res_byte.inner.add(cs, &flag);
+        }
+        result.push(res_byte);
     }
 
     Ok(result)
